fdce3afe2390a7d59c5a76fe5136ac652f3e47be
Dipayn Karmakar <165447252+aspiro-01@users.noreply.github.com>
2024-05-28 00:08:59 -0400

    Adds logic that explain weather its a draw, win, or loss based on humanSlection

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function getComputerChoice() {
     }
 
 }
-let computerChoice = getComputerChoice();
 console.log(getComputerChoice())
 
+// human choice
 let promptMsg = prompt("Type either rock/paper/scissor to play!");
 function getHumanChoice() {
     promptMsg = promptMsg.toLowerCase()
@@ -24,3 +24,41 @@ function getHumanChoice() {
     
 }
 console.log(getHumanChoice());
+
+// function that will compare the human vs computer choice to find win, loose, or draw in the game.
+// when this function is called it should update the score
+ function playRound(humanChoice, computerChoice) {
+    
+    //rock materials
+    if (humanChoice === "rock" && computerChoice === "rock"){
+        return "Draw!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        return "You Loose!";
+    } else if (humanChoice === "rock" && computerChoice === "scissor"){
+        return "You Win!";
+    }
+
+    // paper materials
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        return "You Win!";
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        return "Draw!";
+    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+        return "You Loose!";
+    }
+
+    //scissor materials 
+    else if (humanChoice === "scissor" && computerChoice === "rock") {
+        return "You Loose!";
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
+        return "You Win!";
+    } else if (humanChoice === "scissor" && computerChoice === "scissor") {
+        return "Draw!";
+    } else {
+        return "Something is wrong!";
+    }
+ }
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+ console.log(playRound(humanSelection, computerSelection));
